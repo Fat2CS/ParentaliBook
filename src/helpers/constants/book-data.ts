66ab66d21ecb86df740b690fc0e7f8ecf9f00df5
Fat2CS <coulibalyfatoumata@hotmail.com>
2023-08-14 22:33:54 +0200

    second

---
+++ src/helpers/constants/book-data.ts
@@ -0,0 +1,44 @@
+export const bookData = ` <url>
+<loc>https://urlz.fr/ndEb</loc>
+<desc>Your book store</desc>
+</url>
+<url>
+<loc>https://www.amazon.fr/s?k=livre+sur+l%27%C3%A9ducation&i=stripbooks&rh=n%3A301061&dc&ds=v1%3A6RbM%2Bh0HiuLWiUS2%2FM2egwSnApC7lHgXgh8ht%2BXfrzA&__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1688999297&ref=sr_ex_n_1</loc>
+<desc>Tous sur comment élever son enfant</desc>
+</url>
+<url>
+<loc>https://urlz.fr/ndEb</loc>
+<content>
+    <book>
+    <url>
+    
+    https://urlz.fr/ndF5</url>
+    <desc>Parent sans s'énerver !</desc>
+    <price>7.90</price>
+    <availability>in stock</availability>
+    </book>
+    <book>
+    <url>https://books.toscrape.com/catalogue/full-moon-over-noahs-ark-an-odyssey-to-mount-ararat-and-beyond_811/index.html</url>
+    <desc>A book by Rick Ansonson about his journey to Mount Ararat</desc>
+    <price>49.43</price>
+    <availability>in stock</availability>
+    </book>
+</content>
+</url>
+<url>
+<loc>https://www.amazon.fr/s?bbn=490062&rh=n%3A301061%2Cn%3A301142%2Cn%3A12641853031%2Cn%3A490062%2Cn%3A14267558031&dc&qid=1688999595&rnid=490062&ref=lp_490062_nr_n_8</loc>
+<content>
+    <book>
+    <url>https://urlz.fr/ndE7</url>
+    <desc>La Méthode Pour Réussir Son Adolescence: Livre pour les Ados Filles sur Comment Surmonter les Défis de la Puberté, Améliorer sa Confiance en Soi et Apprendre à mieux Gérer son Argent</desc>
+    <price>12.90</price>
+    <availability>in stock</availability>
+    </book>
+    <book>
+    <url>https://urlz.fr/ndEa</url>
+    <desc>Entre Maman et Moi - Le Journal Mère Fils créateur de souvenirs: Un carnet intime pour s'amuser, mieux se connaître et renforcer la complicité mère-enfant </desc>
+    <price>56.50</price>
+    <availability>in stock</availability>
+    </book>
+</content>
+</url>`;
