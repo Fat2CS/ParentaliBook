44bf946b235fadb435a839959ff650546293252c
Fat2CS <coulibalyfatoumata@hotmail.com>
2023-08-25 11:25:49 +0200

    login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
               className="text-sm font-semibold leading-6 text-gray-900"
             >
               {/* Log in <span aria-hidden="true">&rarr;</span> */}
-              Log in <span aria-hidden="true">&rsquo;</span>
+              {/* Log in <span aria-hidden="true">&rsquo;</span> */}
             </a>
           </div>
         </nav>
